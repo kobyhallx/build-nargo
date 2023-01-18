84e783f9528d1f7e05fc5b878848f844aaedc1da
Koby <koby@aztecprotocol.com>
2023-01-18 16:15:47 +0100

    fix rev logic

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ else
     git checkout $AZTEC_BACKEND_REV
 fi
 
-AZTEC_CONNECT_REV=$(toml2json aztec_backend/barretenberg_static_lib/Cargo.toml | jq -r .dependencies.barretenberg_wrapper.rev)
+AZTEC_CONNECT_REV=$(toml2json $main_dir/aztec_backend/barretenberg_static_lib/Cargo.toml | jq -r .dependencies.barretenberg_wrapper.rev)
 
 if [[ -d "$AZTEC_CONNECT_DIR" ]]; then
     echo "$AZTEC_CONNECT_DIR exists on your filesystem, using it for build..."
